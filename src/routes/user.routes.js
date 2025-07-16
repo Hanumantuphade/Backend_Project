d7751950d5cd6981db5634201a9941b0c9af91dc
Hanumantuphade <hanumantuphade63@gmail.com>
2025-07-16 15:06:50 +0530

    add loginuser and logoutuser

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,15 +1,23 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"; // Assuming you have a multer setup for file uploads
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js"; // Assuming you have a multer setup for file uploads
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(
   upload.fields([
     { name: "avatar", maxCount: 1 }, // For user avatar
-    { name: "coverImage", maxCount: 1 } // For cover image
+    { name: "coverImage", maxCount: 1 }, // For cover image
   ]),
-  
-  registerUser);
 
+  registerUser
+);
+router.route("/login").post(loginUser);
+
+router.route("/logout").post(verifyJWT, logoutUser);
 export default router;
